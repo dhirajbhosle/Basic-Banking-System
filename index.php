0501719cf287c75f48f9e3974ca3f822b3183a11
Yash Jamkar <103654274+dhirajbhosle@users.noreply.github.com>
2024-02-17 16:47:56 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+   
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+
+
+
+    
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+
+
+    <title>Basic Banking System</title>
+
+  </head>
+
+
+
+  <body>
+
+  <?php
+
+  include 'navbar.php';
+
+  ?>
+
+  
+
+      <div class="container-fluid">
+
+      
+
+            <div class="row intro py-1" style="background-color: black;">
+
+              <div class="col-sm-12">
+
+                <div class="heading text-center my-12">
+
+                  <h3 style="color: white;">Welcome to</h3>
+
+                  <h1 style="color: white;">Basic Banking System</h1>
+
+                </div>
+
+              </div>
+
+             
+
+            </div>
+
+
+
+      
+
+          <br>
+
+          <br>
+
+          <br>
+
+          <h3 class="action" ><span style="font-family:"Lucida Console", "Courier New", monospace,>Our Services </h3></span>
+
+            <div class="column activity text-center">
+
+                  <div class="col-md act">
+
+                   
+
+                    <br>
+
+                    <br>
+
+                    <a href="transfermoney.php"><button style="background-color: white color:black">View Our Customers</button></a>
+
+                  </div>
+
+
+                  <div class="col-md act">                   
+                    <br/>
+                    <a href="transactionhistory.php"><button style="background-color: white color:black">Transfer History</button></a>
+
+                  </div>
+
+
+
+                  
+                  
+
+            </div>
+
+           
+
+      </div>
+
+      <br>
+<br>
+<br>
+<br>
+<br>
+<br>
+      <footer class="text-center mt-5 py-2">
+        <p style="color:black font-style:bold">@copyright all right reserved Dhiraj 2024</p>
+      </footer>
+
+     
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  </body>
---
+++ navbar.php
@@ -0,0 +1,40 @@
+<!-- navbar --> 
+
+<nav class="navbar navbar-expand-md navbar-light bg-light" >
+
+<a class="navbar-brand" href="index.php" style="color: blue;">Basic Banking System</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+
+  <span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+      <ul class="navbar-nav ml-auto ">
+
+        <li class="nav-item">
+          <a class="" href="index.php" style="color: blue;">
+          <button>
+            HOME
+         </button>
+          </a>
+
+        </li>
+
+        <li class="nav-item ml-4">
+          <button>
+          <a class="" href="transfermoney.php" style="color: white;">VIEW CUSTOMERS</a>
+        </button>
+        </li>
+
+        <li class="nav-item ml-4">
+          <button>
+          <a class="" href="transactionhistory.php" style="color: white;">HISTORY</a>
+          </button>
+        </li>
+
+</div>
+
+</nav>
